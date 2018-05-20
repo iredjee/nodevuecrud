0d49388be538470e9add8cd0d0efe24969984e63
Rashad Ibrahimov <i.redjee@gmail.com>
2018-05-21 02:13:14 +0400

    Vue with webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const isProd = mode === 'production';
 
 module.exports = {
-  mode: process.env.NODE_ENV,
+  mode,
   entry: './client/src/index.js',
   output: {
     path: path.resolve(__dirname, 'client', 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
     modules: [
       'node_modules',
       path.resolve(__dirname, 'client', 'src')
     ]
   },
   plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({ template: './client/index.html' })
   ],
-  devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map'
+  devtool: isProd ? 'source-map' : 'eval-source-map',
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+    ]
+  }
 };
