5257ef8480ad40cb66e5b598652cdae8f6886cfb
Rashad Ibrahimov <i.redjee@gmail.com>
2018-05-21 04:44:28 +0400

    Added vue bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from 'components/App';
 import router from './router';
+import styles from './styles';
 
 new Vue({ el: '#root', router, components: { App }, template: '<App/>' });
